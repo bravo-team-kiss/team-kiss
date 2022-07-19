b9982bfce24bcc595e7d2a320ca6ea179c9739e4
manwithaplan-pepsiman@protonmail.com <manwithaplan-pepsiman@protonmail.com>
2022-07-19 18:20:53 -0400

    Created a from and to date

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/requestdata', (req, res) => {
+  const from_date = req.query.from;
+  const to_date = req.query.to;
+
+  res.send({
+    'from date': from_date,
+    'to date': to_date,
+  });
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+/* Data that will most likely be found in sets
+
+Organization
+- By Sensor
+
+*/
