b13c59786f887328afea0046820a6ccd837fa0c2
Aileen Ocampo <aileen.ocampo@teambespin.us>
2022-07-22 00:17:40 -0400

    frontend css update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,35 +3,19 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Search from "./components/Search";
 import Display from "./components/Display";
-import { withStyles } from "material-ui/styles";
-
-const styles = (theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-});
 
 function App() {
   return (
     <div className="App-header">
-      <header>
-        <p>Welcome, Team Kiss.</p>
-      </header>
-      <body className="App-body">
-        <Grid container spacing={5}>
-          <Grid item xs={6}>
-            <Search />
-          </Grid>
-          <Grid item xs={6}>
-            <Display />
-          </Grid>
+      <p>Welcome, Team Kiss.</p>
+      <Grid container spacing={5} className="grid-container">
+        <Grid item xs={6}>
+          <Search />
         </Grid>
-      </body>
+        <Grid item xs={6} className="display-container">
+          <Display />
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- frontend/src/components/Display.js
+++ frontend/src/components/Display.js
@@ -1,26 +1,38 @@
 import "../App.css";
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
 
 function Display() {
   return (
     <div className="display-div">
-      <TextField
-        className="data-textField"
-        fullWidth
-        disabled
-        id="filled-disabled"
-        defaultValue="206 Partial Content (RFC 7233)
-      The server is delivering only part of the resource (byte serving) due to a range header sent by the client. The range header is used by HTTP clients to enable resuming of interrupted downloads, or split a download into multiple simultaneous streams.[15]
-      207 Multi-Status (WebDAV; RFC 4918)
-      The message body that follows is by default an XML message and can contain a number of separate response codes, depending on how many sub-requests were made.[16]
-      208 Already Reported (WebDAV; RFC 5842)
-      The members of a DAV binding have already been enumerated in a preceding part of the (multistatus) response, and are not being included again.
-      226 IM Used (RFC 3229)
-      The server has fulfilled a request for the resource, and the response is a representation of the result of one or more instance-manipulations applied to the current instance.[17]"
-        variant="filled"
-        multiline
-      />
+      <Box className="data-textField">
+        <p>
+          Lorem ipsum dolor sit amet, sit sanctus repudiare id, primis convenire
+          vituperatoribus mel ei. Labore albucius mel te, dicta integre
+          persequeris an cum. Discere mentitum intellegebat et has. Vim solum
+          eloquentiam in, per ne erant nobis quidam. Voluptaria suscipiantur has
+          eu. In eos odio vidisse, vel vidit semper constituam id, mea dicit
+          neglegentur no. Id dicant admodum officiis eam. Eu eam malis tantas
+          consequuntur, cu ius tritani facilisis accommodare. Tritani
+          signiferumque ut mea. Ne altera detraxit expetenda duo, in pro erat
+          impetus voluptua, albucius maluisset te sit. At eum diam equidem
+          euripidis. Alterum definiebas theophrastus eu his. Liber persius
+          detraxit ea sed. Sit quem vero quaeque ne. Eu explicari contentiones
+          vel, altera dolores commune cu ius, vis accusam pericula cotidieque
+          ei. Pri ne euripidis dissentiet intellegebat, his dictas definitiones
+          ut. Duo id dico persius, probo facer principes his an. Sed ad solet
+          accusata, ne cum dicit abhorreant. Per et wisi minim platonem. Id
+          utroque copiosae liberavisse qui, alia ipsum abhorreant et duo. Ubique
+          causae suscipit duo at. Cum legere accusam cotidieque ea, dictas
+          iudicabit an pri. Ei per prima tempor, id omnes complectitur sed. Enim
+          accusata accusamus has ut, minimum sapientem ut cum. Albucius
+          sapientem no qui. Duo ea platonem constituto appellantur, atqui
+          delectus cum ea. Ei fabulas deserunt eam, vel aperiri probatus
+          recteque ad. Ius prima autem copiosae eu, no pro vivendo lobortis,
+          legere intellegat no pro.
+        </p>
+      </Box>
     </div>
   );
 }
